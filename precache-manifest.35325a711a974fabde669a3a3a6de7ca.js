42173de384cb301028d72f2f3cc71e4de5f43308
Guillaume Mignot <guillaumeminio@gmail.com>
2022-03-16 19:24:24 +0100

    Updates

---
+++ precache-manifest.35325a711a974fabde669a3a3a6de7ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9548bb5bae4db7b4f6f63dd12cda8e",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "0c0548245f6bbee91297",
+    "url": "/blindtest-spotify/static/css/main.12217c1b.chunk.css"
+  },
+  {
+    "revision": "99291f00ffc51cf5bd8a",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js"
+  },
+  {
+    "revision": "8dfbd160926ae814ef4bd67a82331fc7",
+    "url": "/blindtest-spotify/static/js/2.98663aa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c0548245f6bbee91297",
+    "url": "/blindtest-spotify/static/js/main.5ba3b2aa.chunk.js"
+  },
+  {
+    "revision": "01c4d54207b3c1322762",
+    "url": "/blindtest-spotify/static/js/runtime-main.e8875b69.js"
+  },
+  {
+    "revision": "ed34bc26e2e33c49af23c6c44c6bdee9",
+    "url": "/blindtest-spotify/static/media/loading.ed34bc26.svg"
+  },
+  {
+    "revision": "6a5812940578a284a5256eab66eddac5",
+    "url": "/blindtest-spotify/static/media/logo.6a581294.svg"
+  }
+]);
